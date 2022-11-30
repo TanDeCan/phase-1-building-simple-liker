3453147fe28a5194887da06f225f6c092953a97d
VictoriaBladow <vbladow9999@yahoo.com>
2022-11-29 19:08:15 -0600

    commit tree c851176425fcbf2f71449e31d7211fa637359a63

--- main.js
+++ main.js
@@ -4,8 +4,42 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const hearts = document.getElementsByClassName('like-glyph')
+Array.from(hearts).forEach((el) => {
+  el.addEventListener('click', (e) =>{
+    mimicServerCall()
+    .then(() => {
+      color(e.target)
+    })
+    .catch(() => {
+      displayError()
+    })
+  })
+})
 
+function color(ele) {
+  if(ele.textContent === EMPTY_HEART) {
+    ele.textContent = FULL_HEART
+    ele.className = 'activated-heart'
+  } else {
+    ele.textContent = EMPTY_HEART;
+    ele.classList.remove('activated-heart')
+  }
+  //check text content either empty or full (const)
+  //remove full, change text content, remove activated.class
+  //activate empty, change text content, .activate class
+  // if else statement
+}
 
+function displayError(elem){
+  const modal = document.querySelector('#modal')
+  modal.classList.remove('hidden')
+  setTimeout(() => {
+    modal.classList.add('hidden')
+  }, 3000)
+  //from className hidden from modal 
+  // setTimeout(3)
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
